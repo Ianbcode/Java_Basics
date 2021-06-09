10afebe4a856b20470309018d4d893635b69352c
Ianbcode <ianbecker12@yahoo.com>
2021-06-09 00:06:56 -0700

    writing function file added

--- main/run_main.java
+++ main/run_main.java
@@ -1,6 +1,10 @@
+package main;
 //project 1
+
 public class run_main{
     public static void main(String[] args) {
         System.out.println("Hello World");
+        writing_script.helloWorld();
+        writing_script.helloMars();
     }
 }
---
+++ main/writing_script.java
@@ -0,0 +1,12 @@
+package main;
+public class writing_script {
+
+    public static void helloWorld() {
+        System.out.println("Hello World From Another World");
+
+    }
+    public static int helloMars() {
+        System.out.println("Hello World From Mars");
+        return 5;
+    }
+}
