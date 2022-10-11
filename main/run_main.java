58c57f7424e5b7cfb958fd44dbcfb41078eae907
Ian Becker <39637301+Ianbcode@users.noreply.github.com>
2022-10-11 00:27:21 -0700

    Added Descriptions

--- main/run_main.java
+++ main/run_main.java
@@ -3,9 +3,13 @@ package main;
 
 public class run_main{
     public static void main(String[] args) throws Exception{
+        //Normal Print Statement
         System.out.println("Hello World");
+        //Creating another Class instance
+        //Call method 1, Call method 2, Print method 2 return value
         writing_script writing = new writing_script();
         writing.helloWorld();
-        //writing_script.helloMars();
+        writing_script.helloMars();
+        System.out.println(writing_script.helloMars());
     }
 }
